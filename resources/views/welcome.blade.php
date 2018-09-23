e14b60dd78e8e4e2f23820e7b6ca3a916b240f0b
Akiyoshi <akiyoshi.git@gmail.com>
2018-09-23 17:47:24 +0000

    correct welcome text-conter

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -21,7 +21,7 @@
         </div>
     @else
         <div class="center jumbotron">
-            <div　class="text-center">
+            <div class="text-center">
                 <h1>Welcome to the Microposts</h1>
                 {!! link_to_route('signup.get', 'Sign up now!', null, ['class' => 'btn btn-lg btn-primary']) !!}
             </div>
