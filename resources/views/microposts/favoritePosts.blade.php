9721f327ec018f3549932b398f68288aa75fcd69
Akiyoshi <akiyoshi.git@gmail.com>
2018-09-24 15:15:00 +0000

    view correction in followings,followers,favoritePosts

--- resources/views/microposts/favoritePosts.blade.php
+++ resources/views/microposts/favoritePosts.blade.php
@@ -15,7 +15,7 @@
         </aside>
         <div class="col-xs-8">
             <ul class="nav nav-tabs nav-justified">
-                <li role="presentation" class="{{ Request::is('users/' . $user->id) ? 'active' : '' }}"><a href="{{ route('users.show', ['id' => $user->id]) }}">TimeLine <span class="badge">{{ $count_microposts }}</span></a></li>
+                <li role="presentation" class="{{ Request::is('users/' . $user->id) ? 'active' : '' }}"><a href="{{ route('users.show', ['id' => $user->id]) }}">Microposts <span class="badge">{{ $count_microposts }}</span></a></li>
                 <li role="presentation" class="{{ Request::is('users/*/followings') ? 'active' : '' }}"><a href="{{ route('users.followings', ['id' => $user->id]) }}">Followings <span class="badge">{{ $count_followings }}</span></a></li>
                 <li role="presentation" class="{{ Request::is('users/*/followers') ? 'active' : '' }}"><a href="{{ route('users.followers', ['id' => $user->id]) }}">Followers <span class="badge">{{ $count_followers }}</span></a></li>
                 <li role="presentation" class="{{ Request::is('users/*/favoritePosts') ? 'active' : '' }}"><a href="{{ route('posts.favorite', ['id' => $user->id]) }}">Favorites <span class="badge">{{ $count_favoritePosts }}</span></a></li>
